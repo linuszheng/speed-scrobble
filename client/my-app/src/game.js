e3ef25a2940e8a158c6577223d858bb3c5ac8305
Linus Zheng <linus.zheng@warriorlife.net>
2022-01-01 14:08:50 -0800

    smooth sailing

--- client/my-app/src/game.js
+++ client/my-app/src/game.js
@@ -66,7 +66,7 @@ class WordInput extends React.Component {
                 onKeyPress={ e => {this.handleKeyPress(e)} }
                 onChange={ e => {this.handleInputChange(e)} }>
             </input>
-            <button id="wordSubmit" onClick={ e => {this.handleSubmit()} }>Submit</button>
+            <button id="wordSubmitButton" onClick={ e => {this.handleSubmit()} }>Submit</button>
         </div>);
     }
 }
@@ -84,7 +84,8 @@ class Game extends React.Component {
 
     handleBoard(data){
         this.setState({
-            tiles: data.board.tiles
+            tiles: data.board.tiles,
+            players: data.players
         });
     }
 
@@ -103,17 +104,45 @@ class Game extends React.Component {
         });
     }
 
+    renderWordComponent(word, index){
+        const letterComponents = [];
+        for(const i in word){
+            letterComponents.push(<div key={i} className='tile'>{word[i]}</div>);
+        }
+        return (<div key={index} className="wordComponent">{letterComponents}</div>);
+    }
+
+    renderPlayerBoard(player){
+        const wordComponents = [];
+        for(const i in player.words){
+            wordComponents.push(this.renderWordComponent(player.words[i], i));
+        }
+        return (
+            <div key={player.socketId} className="playerBoard">
+                <div className="playerBoardLabel">{player.socketId}</div>
+                {wordComponents}
+            </div>
+        );
+    }
+
     render(){
+        const playerBoardComponents = [];
+        for(const i in this.state.players){
+            playerBoardComponents.push(this.renderPlayerBoard(this.state.players[i]));
+        }
         return (
             <div id="container">
                 <div id="titleBar">
                     <div id="idLabel">{this.props.id}</div>
                 </div>
-                <Board tiles={this.state.tiles} handleTileClick={ (tileId)=>{this.handleTileClick(tileId)} }/>
-                <div id="playerBoardContainer">
-
+                <div id="allBoardContainer">
+                    <Board tiles={this.state.tiles} handleTileClick={ (tileId)=>{this.handleTileClick(tileId)} }/>
+                    <div id="playerBoardContainer">
+                        {playerBoardComponents}
+                    </div>
                 </div>
                 <WordInput handleWordInputSubmit={ (word)=>{this.handleWordInputSubmit(word)} } />
+                <button id="restartButton" onClick={ ()=>{this.props.emitters.emitUserRestart()} }>Restart</button>
             </div>
         );
     }
